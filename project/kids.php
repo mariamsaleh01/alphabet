9e01a97e6ed5a3af2fa6b1c0645fb5ca6bf4603a
mariamsaleh01 <74983841+mariamsaleh01@users.noreply.github.com>
2020-11-24 21:18:45 +0300

    Add files via upload

---
+++ project/kids.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+  <meta charset="utf-8" />
+      <link href="css/cssfile.css"
+        rel="stylesheet" type="text/css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+  <title>children's education program</title>
+    
+</head>
+<body background='images/bcg.jpg'>
+	<center>
+	<h1>Welcome to our website </h1>
+        <h2>where we will teach your child the Alphabet in the easiest way</h2>
+        <h3>click on a letter for a surprise!</h3>
+	<br>
+        <div class="card-container"><a href="https://youtu.be/gsb999VSvh8" target="_self"><img src="images/a.jpg" alt="none" ></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/kzzXROKd-i0" target="_self"><img src="images/b.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/1dhzPuT6jm0" target="_self"><img src="images/c.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/nb8DqaQmNWg" target="_self"><img src="images/d.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/beaUUPPUT2Y" target="_self"><img src="images/e.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/gVJQL1E7BFQ" target="_self"><img src="images/f.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/0KXtxIiQ7gk" target="_self"><img src="images/g.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/NtUSMBzacQ0" target="_self"><img src="images/h.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/P56hZEhqFCw" target="_self"><img src="images/i.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/6KXX6fCKWes" target="_self"><img src="images/j.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/OGVbUgqp7LQ" target="_self"><img src="images/k.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/qEXMoeYe47c" target="_self"><img src="images/l.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/Nvn9QvV7Aqk" target="_self"><img src="images/m.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/qE5HEeoVGb0" target="_self"><img src="images/n.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/oWbY5EKys60" target="_self"><img src="images/o.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/-v1fg2Hp63s" target="_self"><img src="images/p.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/NKAookrRV4s" target="_self"><img src="images/q.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/gUSJeivdEH8" target="_self"><img src="images/r.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/uSVzk2pqWB4" target="_self"><img src="images/s.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/HHEqOLZ0hr4" target="_self"><img src="images/t.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/nPJRhEV-kF8" target="_self"><img src="images/u.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/PA47cP88ySw" target="_self"><img src="images/v.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/MbUIYnDZZ-M" target="_self"><img src="images/w.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/RX9Pm9qj_QY" target="_self"><img src="images/x.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/L8PdL8ydI28" target="_self"><img src="images/y.jpg" alt="none"></a></div>
+        
+        <div class="card-container"><a href="https://youtu.be/HysVxhemAe4" target="_self"><img src="images/z.jpg" alt="none"></a></div>
+ </center></body></html>
+
+
